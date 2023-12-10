688d742ad04c315fcbea39cb9eecc8835444e76d
XEGENDvy <vishalsyadav4@gmail.com>
2023-12-10 22:44:23 +0530

    /media/ changed to media/

--- hello/settings.py
+++ hello/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIR = {
 }
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'public/static')
-MEDIA_URL='media/'
+MEDIA_URL='/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
