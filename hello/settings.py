0df08b7848a895f1fea97e3dd029c34afde25c67
Vishal Yadav <114361635+XEGENDvy@users.noreply.github.com>
2023-12-11 13:58:44 +0530

    Update settings.py

--- hello/settings.py
+++ hello/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -149,7 +150,7 @@ STATICFILES_DIRS = [
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER ='spadeftw2@gmail.com'
-EMAIL_HOST_PASSWORD ='zrsuaynsgufiidpt'
+EMAIL_HOST_PASSWORD = os.environ.get('GMAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
